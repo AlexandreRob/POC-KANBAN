a32c401d596c1d79af177ce55b1781d2ee127346
AlexandreRob <alexandre.robert974@hotmail.fr>
2023-03-30 11:11:43 +0400

    Update serializer.py
    Ajout d'un serializer pour le model Tableau

--- src/api/serializer.py
+++ src/api/serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, Tableau
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta :
         model = Task
+        fields = '__all__'
+
+class TableauSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Tableau
         fields = '__all__'
