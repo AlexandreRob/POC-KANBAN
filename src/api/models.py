761d9ceda98daf1fdb5b809f13f1411ce14a3578
AlexandreRob <alexandre.robert974@hotmail.fr>
2023-03-30 11:13:23 +0400

    Update models.py
    update Task pour qu'il soit adapté au brief
    add Tableau

--- src/api/models.py
+++ src/api/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-class Task(models.Model):
+class Tableau(models.Model):
+    id_tableau = models.AutoField(primary_key=True)
     titre = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False, blank=True, null=True)
+    position_col = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.titre
+
+class Task(models.Model):
+    id_tache = models.AutoField(primary_key=True)
+    titre = models.CharField(max_length=200)
+    position_col = models.IntegerField(unique=True)
+    tableau = models.ForeignKey(Tableau, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.titre
