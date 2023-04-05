a4183faac06fb13b479eb4e60b8fc45b906d1a47
AlexandreRob <alexandre.robert974@hotmail.fr>
2023-04-05 22:29:59 +0400

    Update models.py
    Update du model pour qu'il soit plus adapté au projet

--- src/api/models.py
+++ src/api/models.py
@@ -5,7 +5,10 @@ from django.db import models
 class Tableau(models.Model):
     id_tableau = models.AutoField(primary_key=True)
     titre = models.CharField(max_length=200)
-    position_col = models.IntegerField(unique=True)
+    position = models.IntegerField()
+    
+    class Meta:
+        ordering = ['position']
 
     def __str__(self):
         return self.titre
@@ -13,8 +16,12 @@ class Tableau(models.Model):
 class Task(models.Model):
     id_tache = models.AutoField(primary_key=True)
     titre = models.CharField(max_length=200)
-    position_col = models.IntegerField(unique=True)
+    position_col = models.IntegerField()
     tableau = models.ForeignKey(Tableau, on_delete=models.DO_NOTHING)
-
+    class Meta:
+        # permet d'assurer que la combinaison de kanban et position soit unique
+        unique_together = ('tableau', 'position_col')
+        
     def __str__(self):
         return self.titre
+    
