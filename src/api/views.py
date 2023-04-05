08c437e15ff4ad0c9d570e0f612a89543f3f9ce5
AlexandreRob <alexandre.robert974@hotmail.fr>
2023-04-05 22:35:02 +0400

    Update views.py
    Mise en commentaire des fonction pour utiliser le viewset seulement
    Ajout de fonction pour avoir les tableau dans un ordre

--- src/api/views.py
+++ src/api/views.py
@@ -9,92 +9,102 @@ from rest_framework import viewsets
 
 # Create your views here.
 
-class TaskViewset(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-
-
 class TableauViewset(viewsets.ModelViewSet):
-    queryset = Tableau.objects.all()
     serializer_class = TableauSerializer
 
-@api_view(['GET'])
-def apiOverview(request):
-    api_urls = {
-        'Liste des tache' : 'task-list/',
-        'Liste des tableau' : 'tableau-list/',
-        'Detail tache' : 'task-detail/<str:pk>/',
-        'Detail tableau' : 'tableau-detail/<str:pk>/',
-        'Create task': 'task-create/',
-        'Create tableau': 'tableau-create/',
-        'Update task': 'task-uptdate/<str:pk>/',
-        'Update tableau': 'tableau-update/<str:pk>/',
-        'Delete': 'task-delete/<str:pk>/'
-        }
+    def get_queryset(self):
+        return Tableau.objects.all().order_by('position')
     
-    return Response(api_urls)
+class TaskViewset(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
 
-@api_view(['GET'])
-def taskList(request):
-    task = Task.objects.all()
-    serializer = TaskSerializer(task, many=True)
-    return Response(serializer.data)
+    def get_queryset(self):
+        board_id = self.request.query_params.get('tableau', None)
+        if board_id is not None:
+            return Task.objects.filter(id_tache=board_id)
 
-@api_view(['GET'])
-def tableauList(request):
-    tableau = Tableau.objects.all()
-    serializer = TableauSerializer(tableau, many=True)
-    return Response(serializer.data)
+        return Task.objects.all()
 
 
-@api_view(['GET'])
-def taskDetail(request, pk):
-    tasks = Task.objects.get(id_tache=pk)
-    serializer = TaskSerializer(tasks, many=False)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def tableauDetail(request, pk):
-    tableau = Tableau.objects.get(id_tableau=pk)
-    serializer = TableauSerializer(tableau, many=False)
-    return Response(serializer.data)
 
 
-@api_view(['POST'])
-def taskCreate(request):
-    serializer = TaskSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def apiOverview(request):
+#     api_urls = {
+#         'Liste des tache' : 'task-list/',
+#         'Liste des tableau' : 'tableau-list/',
+#         'Detail tache' : 'task-detail/<str:pk>/',
+#         'Detail tableau' : 'tableau-detail/<str:pk>/',
+#         'Create task': 'task-create/',
+#         'Create tableau': 'tableau-create/',
+#         'Update task': 'task-uptdate/<str:pk>/',
+#         'Update tableau': 'tableau-update/<str:pk>/',
+#         'Delete': 'task-delete/<str:pk>/'
+#         }
+    
+#     return Response(api_urls)
+
+# @api_view(['GET'])
+# def taskList(request):
+#     task = Task.objects.all()
+#     serializer = TaskSerializer(task, many=True)
+#     return Response(serializer.data)
+
+# @api_view(['GET'])
+# def tableauList(request):
+#     tableau = Tableau.objects.all()
+#     serializer = TableauSerializer(tableau, many=True)
+#     return Response(serializer.data)
 
 
-@api_view(['POST'])
-def tableauCreate(request):
-    serializer = TableauSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def taskDetail(request, pk):
+#     tasks = Task.objects.get(id_tache=pk)
+#     serializer = TaskSerializer(tasks, many=False)
+#     return Response(serializer.data)
+
+# @api_view(['GET'])
+# def tableauDetail(request, pk):
+#     tableau = Tableau.objects.get(id_tableau=pk)
+#     serializer = TableauSerializer(tableau, many=False)
+#     return Response(serializer.data)
 
 
-@api_view(['POST'])
-def taskUpdate(request, pk):
-    task = Task.objects.get(id_tache=pk)
-    serializer = TaskSerializer(instance=task,data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-@api_view(['POST'])
-def tableauUpdate(request, pk):
-    task = Tableau.objects.get(id_tableau=pk)
-    serializer = TableauSerializer(instance=task,data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+# @api_view(['POST'])
+# def taskCreate(request):
+#     serializer = TaskSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
 
 
-@api_view(['DELETE'])
-def taskDeletee(request, pk):
-    task = Task.objects.get(id_tache=pk)
-    task.delete()
-    return Response('Delete fait')
+# @api_view(['POST'])
+# def tableauCreate(request):
+#     serializer = TableauSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+
+
+# @api_view(['POST'])
+# def taskUpdate(request, pk):
+#     task = Task.objects.get(id_tache=pk)
+#     serializer = TaskSerializer(instance=task,data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+
+# @api_view(['POST'])
+# def tableauUpdate(request, pk):
+#     task = Tableau.objects.get(id_tableau=pk)
+#     serializer = TableauSerializer(instance=task,data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+
+
+# @api_view(['DELETE'])
+# def taskDeletee(request, pk):
+#     task = Task.objects.get(id_tache=pk)
+#     task.delete()
+#     return Response('Delete fait')
