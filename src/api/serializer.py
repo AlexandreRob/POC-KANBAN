7e5dc0e18db5017e15636216cd4d53124b111c70
AlexandreRob <alexandre.robert974@hotmail.fr>
2023-04-05 22:32:56 +0400

    Update serializer.py
    Ajout items pour inclure la listes des Tasks

--- src/api/serializer.py
+++ src/api/serializer.py
@@ -7,6 +7,8 @@ class TaskSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class TableauSerializer(serializers.ModelSerializer):
+    items = TaskSerializer(many=True, read_only=True)
+    
     class Meta :
         model = Tableau
         fields = '__all__'
