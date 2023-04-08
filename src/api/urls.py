34828647fe748a5ce49db187ac52a23a7f252e93
AlexandreRob <alexandre.robert974@hotmail.fr>
2023-04-08 19:01:58 +0400

    Mise en place du routers

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,15 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.apiOverview, name = 'api-overview'),
-    # path('task-list/', views.taskList, name = 'task-list'),
-    # path('tableau-list/', views.tableauList, name = 'tableau-list'),
-    # path('task-detail/<str:pk>/', views.taskDetail, name = 'task-detail'),
-    # path('tableau-detail/<str:pk>/', views.tableauDetail, name = 'tableau-detail'),  
-    # path('task-create', views.taskCreate, name = 'task-create'),
-    # path('tableau-create', views.tableauCreate, name = 'tableau-create'),
-    # path('task-update/<str:pk>/', views.taskUpdate, name = 'task-update'),
-    # path('tableau-update/<str:pk>/', views.tableauUpdate, name = 'tableau-update'),
-    # path('task-delete/<str:pk>/', views.taskDeletee, name = 'task-delete'),
     
 ]
